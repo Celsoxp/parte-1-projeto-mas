c1356ac5d06bb8681e41074d20109cb4bdd0e91f
Jeam Alves <jeanmalves@gmail.com>
2017-06-11 21:24:17 -0300

    Adicionado metodos e arrays

--- src/br/edu/utfpr/mas/LeArquivo.java
+++ src/br/edu/utfpr/mas/LeArquivo.java
@@ -12,7 +12,9 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,7 +25,7 @@ import java.util.logging.Logger;
  */
 public class LeArquivo {
 
-    private static final String ESPACE = " ";       //#define do c
+    private static final String SPACE = " ";       //#define do c
     private static Calendar calendar = Calendar.getInstance();
 
     /**
@@ -40,10 +42,20 @@ public class LeArquivo {
             BufferedReader buffer = new BufferedReader(new FileReader(fileName));
 
             String line = buffer.readLine();
-            int count = 1;
+            int count = 0;
+            int max = 0;
+            double avg = 0;
+            
+            int[] r = new int[60];
+            int[] cpu = new int[60];
+            int[] swp = new int[60];
+            int[] free = new int[60];
+            int[] bff = new int[60];
+            int[] cache = new int[60];
+            
             while (line != null) {
-                int espace = line.indexOf(ESPACE);
-                String serverName = line.substring(0, espace);
+                int space = line.indexOf(SPACE);
+                String serverName = line.substring(0, space);
                 
                 Date dateTime = getDate(line.substring(9, 28), "yyyy-MM-dd HH:mm:ss");
                 
@@ -53,8 +65,22 @@ public class LeArquivo {
                 int month = getMonth();
                 int year = getYear();
                 String time = getTime(dateTime, "HH:mm:ss");
-
-                if (count == 60) {
+                
+                r[count] = Integer.parseInt(line.substring(29, space));
+                swp[count] = Integer.parseInt(line.substring(37,space));
+                free[count] = Integer.parseInt(line.substring(42, space));
+                bff[count] = Integer.parseInt(line.substring(50, space));
+                cache[count] = Integer.parseInt(line.substring(57, space));
+                cpu[count] = 100 - (Integer.parseInt(line.substring(102, space)));
+                
+                if (count == 59) {
+                    
+                    sb.append(serverName).append(SPACE);
+                    
+                    max = getMax(r);
+                    avg = getAvg(r);
+                    
+                    
                     //soma os totais de max e média.
                     //adiciona no buffer a linha resultante.
                     
@@ -105,5 +131,19 @@ public class LeArquivo {
     
     private int getYear() {
         return calendar.get(Calendar.YEAR);
+    }
+    
+    private int getMax(int[] array) {
+        return Arrays.stream(array).max().getAsInt();
+    }
+    
+    private double getAvg(int[] array) {
+        double total = 0;
+        
+        for (int i = 0; i < array.length; i++) {
+            total += array[i];
+        }
+        
+        return total/array.length;
     }
 }
